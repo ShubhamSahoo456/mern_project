5379c509fddcd7964b70631221e373b2ddd02d62
unknown <shubham.sahoo456@gmail.com>
2021-08-06 01:45:56 +0530

    post register api

--- routing/routers.js
+++ routing/routers.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const User = require('../db/schema');
 const router = express.Router();
 
 router.get("/",(req,res)=>{
     res.send('router js')
 });
 
-router.post("/",(req,res)=>{
-    res.send(req.body);
-    console.log(req.body);
-})
+
+//post method for registration with validation
+router.post("/register",(req,res)=>{
+    const {name,email,phone,work,password,cpassword} = req.body;
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        res.status(422).json({message:"plzz fill all the fields",status:false});
+    }
+
+    User.findOne({email:email})
+    .then((userExists)=>{
+        if(userExists){
+            res.status(500).json({message:"email already exists",status:false});
+        }else{
+            const user = new User({name,email,phone,work,password,cpassword});
+            user.save().then(()=>{
+                res.status(200).json({message:"registered successfully",status:true});
+            }).catch((error)=>{
+                res.status(500).json({message:"user not registered",status:false});
+            })
+        }
+    }).catch((error)=>{
+        console.log(error);
+    });
+});
 
 
 router.get("/aboutus",(req,res)=>{
@@ -23,7 +44,7 @@ router.get("/signin",(req,res)=>{
     res.send('hello this is signin page');
 });
 
-router.get("/signup",(req,res)=>{
+router.get("/register",(req,res)=>{
     res.send('hello this is sign up page');
 });
 

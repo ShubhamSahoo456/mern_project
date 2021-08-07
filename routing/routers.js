b7caf5f8b7efd1e0d8115a76197904e028ab7e4f
unknown <shubham.sahoo456@gmail.com>
2021-08-07 14:12:10 +0530

    async

--- routing/routers.js
+++ routing/routers.js
@@ -8,27 +8,26 @@ router.get("/",(req,res)=>{
 
 
 //post method for registration with validation
-router.post("/register",(req,res)=>{
+router.post("/register",async(req,res)=>{
     const {name,email,phone,work,password,cpassword} = req.body;
     if(!name || !email || !phone || !work || !password || !cpassword){
         res.status(422).json({message:"plzz fill all the fields",status:false});
     }
 
-    User.findOne({email:email})
-    .then((userExists)=>{
-        if(userExists){
-            res.status(500).json({message:"email already exists",status:false});
+    try{
+        const check_email = await User.findOne({email:email});
+        if(check_email){
+            res.status(500).json({message:'email already exists',status:false});
         }else{
             const user = new User({name,email,phone,work,password,cpassword});
-            user.save().then(()=>{
-                res.status(200).json({message:"registered successfully",status:true});
-            }).catch((error)=>{
-                res.status(500).json({message:"user not registered",status:false});
-            })
+            await user.save();
+            res.status(200).json({message:'registered successfully',status:true});
         }
-    }).catch((error)=>{
-        console.log(error);
-    });
+
+    }catch(error){
+        res.send(error);
+    }
+
 });
 
 

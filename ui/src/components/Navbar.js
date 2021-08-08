343529636ca3b0699863de4d3375833b466f434b
unknown <shubham.sahoo456@gmail.com>
2021-08-08 17:17:20 +0530

    dvs

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import { NavLink } from 'react-router-dom';
+import Logo from '../images/IBLONDE.png'
 
 
 const Navbar = ()=>{
@@ -8,7 +9,7 @@ const Navbar = ()=>{
     return(
         <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="/">Navbar</a>
+  <NavLink exact to="/"><img src={Logo} alt=""/></NavLink>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
